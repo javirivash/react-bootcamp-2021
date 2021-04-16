f69b0f04f79d8461a1ceb0c7a99852d059770f62
Javier Rivas <javirivas14@gmail.com>
2021-04-15 21:55:42 -0500

    Implement custom hook to consume context from Search

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const appContext = createContext();
+
+export const useAppContext = () => {
+  const context = useContext(appContext);
+  if (context === undefined) {
+    throw new Error(
+      "useAppContext must be used within AppState component tree"
+    );
+  }
+  return context;
+};
 
 export default appContext;
