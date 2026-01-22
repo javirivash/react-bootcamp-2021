533eda13e2fdaa13de62d3c18206649e74ac09e5
Javier Rivas <64754159+javirivash@users.noreply.github.com>
2026-01-22 02:08:33 -0700

    Take fetch logic to a custom hook - Add spinner component for loading state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-/* global gapi*/
-/*eslint no-undef: "error"*/
-
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./Header";
 import Videos from "./Videos";
+import VideoDetailsView from "./VideoDetailsView";
+import Spinner from "./Spinner";
+import useApiSearch from "../hooks/useApiSearch";
 import styled, { createGlobalStyle } from "styled-components";
-// import data from "../mock/youtube-videos-mock.json";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -15,86 +14,55 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Content = styled.main`
-  padding: 30px;
+const MainTitle = styled.h1`
+  font-family: "Oswald", sans-serif;
+  font-size: 48px;
+  text-align: center;
+  margin: 90px auto 35px;
+`;
 
-  .mainTitle {
-    font-family: "Oswald", sans-serif;
-    font-size: 48px;
-    text-align: center;
-    margin: 60px auto 35px;
-  }
+const Content = styled.main`
+  padding: 0 30px;
 `;
 
 const App = () => {
-  const [searchText, setSearchText] = useState("Wizeline");
-  const [fetchedVideos, setFetchedVideos] = useState([]);
-
-  // eslint-disable-next-line no-undef
-  const apiKey = process.env.REACT_APP_API_KEY;
-  let discovery = {};
-
-  const handleClientLoad = async () => {
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
-      );
-      discovery = await response.json();
-      gapi.load("client", handleClientInit);
-    } catch (error) {
-      console.error("handleClientLoad: Something went wrong... ", error);
-    }
-  };
-
-  const handleClientInit = async () => {
-    try {
-      await gapi.client.init({
-        apiKey: apiKey,
-        discoveryDocs: [discovery],
-      });
-    } catch (error) {
-      console.error("handleClientInit: Something went wrong... ", error);
-    }
-  };
-
-  const searchRequest = async (searchText) => {
-    try {
-      const response = await gapi.client.youtube.search.list({
-        part: ["snippet"],
-        maxResults: 24,
-        q: searchText,
-        type: ["video"],
-      });
-      setFetchedVideos(response.result.items);
-    } catch (error) {
-      console.error("searchRequest: Something went wrong... ", error);
-    }
-  };
-
-  useEffect(() => {
-    handleClientLoad();
-  }, []);
-
-  useEffect(() => {
-    // fetch only when text is not empty && youtube api is
-    searchRequest(searchText);
-  }, [searchText]);
+  const [searchText, setSearchText] = useState("Wizeline Academy");
+  const [resultVideos, loading] = useApiSearch(searchText);
+  const [shouldShowSearch, setShouldShowSearch] = useState(true);
 
   const handleSearch = (text) => {
     setSearchText(text);
   };
 
-  console.log("App fetched: ", fetchedVideos);
+  const handleShowSearch = (shouldShow) => {
+    setShouldShowSearch(shouldShow);
+  };
 
   return (
     <Fragment>
       <GlobalStyle />
       <div className="App">
-        <Header handleSearch={handleSearch} />
-        <Content>
-          <h1 className="mainTitle">Welcome to YouTubit</h1>
-          <Videos fetchedVideos={fetchedVideos} />
-        </Content>
+        <Header
+          handleShowSearch={handleShowSearch}
+          handleSearch={handleSearch}
+        />
+        <MainTitle>Welcome to YouTubit</MainTitle>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Content>
+            {shouldShowSearch ? (
+              <Fragment>
+                <Videos
+                  handleShowSearch={handleShowSearch}
+                  resultVideos={resultVideos}
+                />
+              </Fragment>
+            ) : (
+              <VideoDetailsView name={"javier"} />
+            )}
+          </Content>
+        )}
       </div>
     </Fragment>
   );
