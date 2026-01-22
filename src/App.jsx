d1a043536f3182a3db1b71b98abe07f8b45eed15
Javier Rivas <64754159+javirivash@users.noreply.github.com>
2026-01-22 02:21:24 -0700

    Replace HashRouter for BrowserRouter and fix bug in updateLocalFavorites()

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AlertState from "./context/alert/AlertState";
 import AppState from "./context/app/AppState";
 import styled from "styled-components";
