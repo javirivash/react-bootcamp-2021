c17ed08a6dc741e2a1770844dcabce1b78a0f298
Javier Rivas <64754159+javirivash@users.noreply.github.com>
2026-01-22 02:21:22 -0700

    Replace children components for styled components within VideoItemDetails

--- src/components/VideoItem/_children/VideoItemDetails.test.jsx
+++ src/components/VideoItem/_children/VideoItemDetails.test.jsx
@@ -1,25 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import VideoDetails from "./VideoDetails";
+import VideoItemDetails from "./VideoItemDetails";
 import snippet from "../snippet-mock.json";
 
-describe("VideoDetails", () => {
+describe("VideoItemDetails", () => {
   test("video title is in the document", () => {
-    render(<VideoDetails snippet={snippet} />);
+    render(<VideoItemDetails snippet={snippet} />);
     expect(
       screen.getByRole("heading", { name: "Wizeline", exact: false })
     ).toBeInTheDocument();
   });
 
   test("video channel is in the document", () => {
-    render(<VideoDetails snippet={snippet} />);
+    render(<VideoItemDetails snippet={snippet} />);
     expect(
       screen.getByRole("heading", { name: "Wizeline", exact: false })
     ).toBeInTheDocument();
   });
 
   test("video description is in the document", () => {
-    render(<VideoDetails snippet={snippet} />);
+    render(<VideoItemDetails snippet={snippet} />);
     expect(screen.getByRole("paragraph")).toBeInTheDocument();
   });
 });
